3f55a4090bc8931ce7ae933ba6e22db24fe8bd0d
oppk6kitty-lab <oppk6kitty@gmail.com>
2026-04-17 15:09:25 -0300

    df

---
+++ 0TIMIZACAO/otimizacaoex2.py
@@ -0,0 +1,20 @@
+import numpy as np  
+import  math
+
+
+def f(x):
+    return (x**2 / 10)-2 * np.sin(x)
+
+xl = 0
+xu = 4
+n= 6
+Eppara = (0.5 * 10) ** (2-n)
+
+
+
+phi = 1.6180
+
+n=6
+
+while
+    epest  <== eppara
