3afe6234b0b6f88ff77cc13b9807c243d90834aa
oppk6kitty-lab <oppk6kitty@gmail.com>
2026-05-19 16:57:42 -0300

    1

---
+++ SISTEMA NAO LINEAR/exemplo1.py
@@ -0,0 +1,47 @@
+import numpy as np
+
+#Funcao F(x)
+def f1(x1,x2):
+    return x1**2 - x1*x2 - 10
+
+def f2(x1,x2):
+    return x2 + 3*x1*x2**2 - 57
+
+def df1dx1(x1,x2):
+    return 2*x1 - x2
+
+def df1dx2(x1,x2):
+    return -x1
+
+def df2dx1(x1,x2):
+    return 3*x2**2
+
+def df2dx2(x1,x2):
+    return 1 + 6*x1*x2
+
+Epest = 100
+n = 6
+Eppara = 0.5 * 10**(2-n)
+k = 0
+
+xold = np.array([1.5,3.5])
+xnew = np.array([0,0])
+
+
+while Epest >= Eppara:
+
+    x1 = xold[0]
+    x2 = xold[1]
+
+    xnew[0] = x1 - ((f1(x1,x2) * df2dx2(x1,x2)) - (f2(x1,x2)*df1dx2(x1,x2))) / ((df1dx1(x1,x2)*(df2dx2(x1,x2))-(df1dx2(x1,x2))*(df2dx1(x1,x2))))
+    xnew[1] = x2 - ((f2(x1,x2) * df1dx1(x1,x2)) - (f1(x1,x2)*df2dx1(x1,x2))) / ((df1dx1(x1,x2)*(df2dx2(x1,x2))-(df1dx2(x1,x2))*(df2dx1(x1,x2))))
+
+    Epest = np.max(np.abs((xold - xnew) / xnew)) * 100
+    print(Epest)
+    xold = xnew.copy()
+    k += 1
+    
+
+
+
+    
